9bc6eb04fe36efd4ab44a2f053b10f13c6e2267f
basovakatya11 <106011130+basovakatya11@users.noreply.github.com>
2022-07-15 20:56:17 +0300

    Add files via upload

---
+++ Document4/index.js
@@ -0,0 +1,19 @@
+var expandButton = document.querySelector('.ReadMore');
+var hiddenImages = document.querySelectorAll('.brand-image_hidden');
+buttonText = expandButton.querySelector('.text');
+
+expandButton.addEventListener('click', function () {
+  if (buttonText.textContent === 'Показать все') {
+    for (let i = 0; i < hiddenImages.length; i++) {
+      hiddenImages[i].classList.remove('brand-image_hidden');
+    }
+    buttonText.textContent = 'Скрыть';
+  } else {
+    for (let i = 0; i < hiddenImages.length; i++) {
+      hiddenImages[i].classList.add('brand-image_hidden');
+    }
+    buttonText.textContent = 'Показать все';
+  }
+});
+
+
